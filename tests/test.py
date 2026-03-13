eb9eccbda8ea1148b302057a8ed7c8cdbb6106a9
lizihao <lizihao@cug.edu.cn>
2026-03-13 14:29:08 +0800

    [fix] post模块的导入

--- tests/test.py
+++ tests/test.py
@@ -2,4 +2,4 @@ from pyosis.core.all_func import *
 from pyosis.post import osis_check_result
 
 if __name__ == '__main__':
-    osis_check_result("混凝土","PC腹板斜截面抗裂验算","频遇组合包络")
+    print(osis_check_result("混凝土","PC腹板斜截面抗裂验算","频遇组合包络"))
