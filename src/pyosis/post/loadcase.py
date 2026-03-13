22253093e60d0b5c995aabc5893eeaac3bd21485
myf <myf@example.com>
2026-03-13 10:49:58 +0800

    [fix] 移除osis_elem_force

--- src/pyosis/post/loadcase.py
+++ src/pyosis/post/loadcase.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Literal, Any
-from ..core import OSISEngine, project, command
+from ..core import project, command
 import json
 
 # 将工况结果转换为txt的命令
@@ -8,24 +8,24 @@ _CMD = "/output,{out_file_path},{e_type},{check_file_name}"
 _LOAD_CASE_FILE_PATH = "result"
 _LOAD_CASE_TXT_PATH = "temp"
 
-def osis_elem_force(strLCName: str, eDataItem: Literal['EF'], eElementType: Literal["BEAM3D", "TRUSS", "SPRING", "CABLE", "SHELL"]):
-    '''
-    提取内力结果
-    
-    Args:
-        strLCName (str): 工况名称
-        eDataItem (str): 数据类型，不区分大小写。EF = 内力
-        eElementType (str): 单元类型，不区分大小写。BEAM3D = 梁柱单元，TRUSS = 桁架单元，SPRING = 弹簧单元，CABLE = 拉索单元，SHELL = 壳单元
-
-    Returns:
-        tuple (bool, str):
-            - bool: 操作是否成功
-            - str: 失败原因（如果操作失败）
-    '''
-    e = OSISEngine.GetInstance()
-    eDataItem = eDataItem.upper()
-    eElementType = eElementType.upper()
-    return e.OSIS_ElemForce(strLCName, eDataItem, eElementType)
+# def osis_elem_force(strLCName: str, eDataItem: Literal['EF'], eElementType: Literal["BEAM3D", "TRUSS", "SPRING", "CABLE", "SHELL"]):
+#     '''
+#     提取内力结果
+#
+#     Args:
+#         strLCName (str): 工况名称
+#         eDataItem (str): 数据类型，不区分大小写。EF = 内力
+#         eElementType (str): 单元类型，不区分大小写。BEAM3D = 梁柱单元，TRUSS = 桁架单元，SPRING = 弹簧单元，CABLE = 拉索单元，SHELL = 壳单元
+#
+#     Returns:
+#         tuple (bool, str):
+#             - bool: 操作是否成功
+#             - str: 失败原因（如果操作失败）
+#     '''
+#     e = OSISEngine.GetInstance()
+#     eDataItem = eDataItem.upper()
+#     eElementType = eElementType.upper()
+#     return e.OSIS_ElemForce(strLCName, eDataItem, eElementType)
 
 def osis_load_case_result(fileName:str, eType:Literal['LCEF','LCED','LCND','LCBF','LCTL','LCS']) -> tuple[bool, str, Any]:
     """
